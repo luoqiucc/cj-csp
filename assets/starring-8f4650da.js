2f4b240c326bfaac784680d251430c1a2202978c
CI <CI@example.com>
2023-02-19 06:05:21 +0000

    deploy

---
+++ assets/starring-8f4650da.js
@@ -0,0 +1,1 @@
+const a=[{user:"影歌",avatar:"/images/avatar/avatar_shadowsong.jpg"},{user:"永远滴神",avatar:"/images/avatar/avatar_kami_forever.jpg"},{user:"前排观众",avatar:"/images/avatar/avatar_dollar.jpg"}];export{a as s};
