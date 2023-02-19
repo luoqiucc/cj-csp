9646c8875edc83ac8756af02c715d6977327f121
影歌Shadowsong <ombrechanson@outlook.com>
2023-02-19 14:03:32 +0800

    添加两期内容

--- src/data/c202103.js
+++ src/data/c202103.js
@@ -11,21 +11,383 @@ const c202103 = {
             session: [
                 {
                     ...starring[0],
-                    message: '早上好',
+                    message: '我觉得我是个女生，但我有异装癖，想穿男装，我又是个同性恋，喜欢女人，但我应该可以进女厕所，我还信教，是个原教旨主义者，不能去做变性手术，我是个黑人，得了白化病，又晒了太阳，所以我又有点黄？我还是个未成年人，但是由于一次意外强辐射导致身体加速发育，看起来比实际年龄大，我是素食主义者，但是由于罕见疾病，一顿不肉就会死，所以我还逼迫自己强行顿顿吃肉，因此还患上了重度抑郁症',
                     image: ''
                 },
                 {
                     ...starring[1],
-                    message: '下午好',
+                    message: '不愧是你',
+                    image: ''
+                }
+            ]
+        },
+        {
+            title: '',
+            author: '',
+            session: [
+                {
+                    ...starring[0],
+                    message: '色图time',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '嘿嘿',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '嘿嘿',
                     image: ''
                 },
                 {
                     ...starring[2],
-                    message: '晚上好',
+                    message: '嘿嘿',
                     image: ''
                 }
             ]
-        }
+        },
+        {
+            title: '',
+            author: '',
+            session: [
+                {
+                    ...starring[0],
+                    message: '【KAROMIX (karory)】 三玖が既成事実を作る本 (五等分の花嫁)',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '太好了',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '开冲',
+                    image: ''
+                }
+            ]
+        },
+        {
+            title: '',
+            author: '',
+            session: [
+                {
+                    ...starring[0],
+                    message: '',
+                    image: '/images/magazine/c202103/c202103_1.png'
+                },
+                {
+                    ...starring[0],
+                    message: '有无懂哥',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '这几个是谁',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '中间那个突然想不起来了',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '我去问问董哥',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '看来是画上去的',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '。。。',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '懂哥也不知道',
+                    image: ''
+                }
+            ]
+        },
+        {
+            title: '',
+            author: '',
+            session: [
+                {
+                    ...starring[1],
+                    message: '玩铁拳最厉害的人 最终败在了铁拳上',
+                    image: ''
+                }
+            ]
+        },
+        {
+            title: '',
+            author: '',
+            session: [
+                {
+                    ...starring[1],
+                    message: '现在平台总是想方设法的获取你的信息',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '然后转手卖给别人',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '搞得游戏都没法玩了',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '确实',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '我需要法律来保护自己',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '这已经快上升到人生安全了',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '说不定哪天 FBI给你拷走了，说你在美国犯罪了',
+                    image: ''
+                }
+            ]
+        },
+        {
+            title: '',
+            author: '',
+            session: [
+                {
+                    ...starring[1],
+                    message: '我已经不用WeGame了',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '我已经不用WeGame了',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '看见腾讯就烦',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '那玩意还巨卡',
+                    image: ''
+                }
+            ]
+        },
+        {
+            title: '',
+            author: '',
+            session: [
+                {
+                    ...starring[1],
+                    message: '明天',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '啊，这',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '去',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '啊，这',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '报个',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '啊，这',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '班',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '啊，这',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '学习',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '啊，这',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '法律',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '啊，这',
+                    image: ''
+                }
+            ]
+        },
+        {
+            title: '',
+            author: '',
+            session: [
+                {
+                    ...starring[1],
+                    message: '孩子们请记住 如果她是演员的话就不是妓女 听懂掌声',
+                    image: ''
+                }
+            ]
+        },
+        {
+            title: '',
+            author: '',
+            session: [
+                {
+                    ...starring[1],
+                    message: 'bilibili真是学习平台😁😁',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '各种学习资料',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '真人一对一辅导',
+                    image: ''
+                },
+                {
+                    ...starring[2],
+                    message: '对，还有教学穿衣的，从内衣开始🧐',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '哈哈哈',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '确实',
+                    image: ''
+                },
+            ]
+        },
+        {
+            title: '',
+            author: '',
+            session: [
+                {
+                    ...starring[0],
+                    message: '今年上半年最新的女优热度排名',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '现在看的少了，可多人我都不认识了',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '确实',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '终于要渐行渐远了吗',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '哎',
+                    image: ''
+                }
+            ]
+        },
+        {
+            title: '',
+            author: '',
+            session: [
+                {
+                    ...starring[1],
+                    message: '这个一般',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '我还是喜欢大姐姐',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '确实',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '我也喜欢大姐姐',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '快给哥几个整点',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '大 姐姐',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '够大吧',
+                    image: ''
+                }
+            ]
+        },
+        {
+            title: '',
+            author: '',
+            session: [
+                {
+                    ...starring[0],
+                    message: '我来给大家科普一下，少儿频道是给少儿看的频道，不是看少儿的频道',
+                    image: ''
+                }
+            ]
+        },
     ]
 }
 
--- src/data/c202201.js
+++ src/data/c202201.js
@@ -11,17 +11,27 @@ const c202201 = {
             session: [
                 {
                     ...starring[0],
-                    message: 'hello',
+                    message: '给各位看看我上课都带啥',
                     image: ''
                 },
                 {
                     ...starring[0],
-                    message: 'hello',
+                    message: '“优秀的程序员往往只需要最简单的工具”',
                     image: ''
                 },
                 {
                     ...starring[1],
-                    message: 'hello',
+                    message: '优秀的人上班什么都不带',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '你搁这搁这呢',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '6',
                     image: ''
                 }
             ]
@@ -32,18 +42,261 @@ const c202201 = {
             session: [
                 {
                     ...starring[0],
-                    message: '你好',
+                    message: '隐藏在阴影中的我啊',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '让我在任性一次吧',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '我这两天精神状态有点爆炸',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '太长时间没有色图了',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '忍不了了',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '想家了',
+                    image: ''
+                }
+            ]
+        },
+        {
+            title: '',
+            author: '',
+            session: [
+                {
+                    ...starring[1],
+                    message: '兄弟们',
                     image: ''
                 },
                 {
                     ...starring[1],
-                    message: '你好',
+                    message: '找了个妹妹 天天陪我打游戏',
                     image: ''
                 },
                 {
-                    ...starring[2],
-                    message: '你好',
-                    image: '/images/magazine/c202201/c202201_img.jpg'
+                    ...starring[1],
+                    message: '哈哈哈',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '',
+                    image: '/images/magazine/c202201/c202201_1.jpg'
+                },
+                {
+                    ...starring[1],
+                    message: '这波是 我先上车了',
+                    image: ''
+                }
+            ]
+        },
+        {
+            title: '',
+            author: '',
+            session: [
+                {
+                    ...starring[1],
+                    message: '哎',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '声音还怪好听的',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '给你们听听',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: 'TMD',
+                    image: ''
+                }
+            ]
+        },
+        {
+            title: '',
+            author: '',
+            session: [
+                {
+                    ...starring[1],
+                    message: '声音还不错吧',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '我让她给我配的闪电风暴',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '真不错(极不情愿)',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '确实好听啊(极不情愿)',
+                    image: ''
+                }
+            ]
+        },
+        {
+            title: '',
+            author: '',
+            session: [
+                {
+                    ...starring[0],
+                    message: '刚才吃了一个茶叶蛋味的茶叶蛋',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '真不错',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '你们知道茶叶蛋味的茶叶蛋是什么味儿的吗',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '其实是茶叶蛋味儿的',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '听君一席话盛似一句话',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '',
+                    image: '/images/magazine/c202201/c202201_2.png'
+                }
+            ]
+        },
+        {
+            title: '',
+            author: '',
+            session: [
+                {
+                    ...starring[1],
+                    message: '一个男的挺着老二走进女澡堂，告诉赤身裸体的女人说，我的身体是男的，内心是女的，这就是欧美的政治正确！这男的阉了才能进女澡堂，这是中国人的常识。​',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '到位',
+                    image: ''
+                }
+            ]
+        },
+        {
+            title: '',
+            author: '',
+            session: [
+                {
+                    ...starring[1],
+                    message: '这不是',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '这是',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '我跟你说先。。。。然后。。。。在。。。。就好了',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '',
+                    image: '/images/magazine/c202201/c202201_3.png'
+                },
+                {
+                    ...starring[1],
+                    message: '简简单单',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '这不1+1=2吗',
+                    image: ''
+                }
+            ]
+        },
+        {
+            title: '',
+            author: '',
+            session: [
+                {
+                    ...starring[0],
+                    message: '完了',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '数据是空的',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '嗯',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '看来要编实验结果了',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '牛',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '数据造假我在行',
+                    image: ''
+                },
+                {
+                    ...starring[1],
+                    message: '你除了色图就没有真的数据',
+                    image: ''
+                }
+            ]
+        },
+        {
+            title: '',
+            author: '',
+            session: [
+                {
+                    ...starring[0],
+                    message: '记住我说的话',
+                    image: ''
+                },
+                {
+                    ...starring[0],
+                    message: '《对象？废物才需要这东西》',
+                    image: ''
                 }
             ]
         }
